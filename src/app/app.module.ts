fa2b1fb9889276c152b7fcd18e628aa735db7606
alhammialiff <alhammi.aliff@gmail.com>
2022-11-12 14:16:47 +0800

    Created Angular Reactive Form in Contact Component Feedback Form

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -15,6 +15,9 @@ import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule} from '@angular/material/input';
 import { MatCheckboxModule } from '@angular/material/checkbox';
 import { FormsModule } from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
+import { MatSelectModule } from '@angular/material/select';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
 
 import { AppComponent } from './app.component';
 import { MenuComponent } from './menu/menu.component';
@@ -54,6 +57,7 @@ import { LoginComponent } from './login/login.component';
     BrowserAnimationsModule,
     FlexLayoutModule,
     FormsModule,
+    ReactiveFormsModule,
     MatToolbarModule,
     MatListModule,
     MatGridListModule,
@@ -63,7 +67,9 @@ import { LoginComponent } from './login/login.component';
     MatDialogModule,
     MatFormFieldModule,
     MatInputModule,
-    MatCheckboxModule
+    MatCheckboxModule,
+    MatSelectModule,
+    MatSlideToggleModule
   ],
 
   providers: [
--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validator } from '@angular/forms';
+import { Feedback, ContactType } from '../shared/feedback';
+
 
 @Component({
   selector: 'app-contact',
@@ -7,9 +10,39 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ContactComponent implements OnInit {
 
-  constructor() { }
+  feedbackForm: FormGroup;
+  feedback: Feedback;
+  contactType = ContactType;
+
+  constructor(private fb: FormBuilder) { 
+    // Instantiate creation of form
+    this.createForm();
+  }
 
   ngOnInit() {
+  }
+
+  createForm(){
+
+    // Create form with specified structure
+    this.feedbackForm = this.fb.group({
+      firstname: '',
+      lastname: '',
+      telnum: 0,
+      email: '',
+      agree: false,
+      contacttype: 'None',
+      message: ''
+    });
+
+  }
+
+  onSubmit(){
+    // Because it so happens that we created the feedbackForm structure identical to the datatype structure,
+    // We are able to map the this.feedback var with the feedbackForm.value like so (no frills)
+    this.feedback = this.feedbackForm.value;
+    console.log(this.feedback);
+    this.feedbackForm.reset();
   }
 
 }
