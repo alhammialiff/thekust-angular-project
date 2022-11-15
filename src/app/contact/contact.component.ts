709301723f71408d8479eea67e000d9ce8976836
alhammialiff <alhammi.aliff@gmail.com>
2022-11-15 14:27:45 +0800

    Migrated form validations from template to component ts file (contact.component.ts)

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -1,6 +1,7 @@
 // ViewChild allows viewing of child elements in the DOM
 import { Component, OnInit, ViewChild } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { controlNameBinding } from '@angular/forms/src/directives/reactive_directives/form_control_name';
 import { Feedback, ContactType } from '../shared/feedback';
 
 
@@ -15,6 +16,35 @@ export class ContactComponent implements OnInit {
   feedback: Feedback;
   contactType = ContactType;
 
+  // Helps to tracks any errors in the form
+  formErrors = {
+    'firstname': '',
+    'lastname': '',
+    'telnum': '',
+    'email': ''
+  };
+
+  validationMessages = {
+    'firstname': {
+      'required': 'First name is required',
+      'minlength': 'First name must be at least 2 characters long',
+      'maxlength': 'First name cannot be more than 25 characters long'
+    },
+    'lastname': {
+      'required': 'First name is required',
+      'minlength': 'First name must be at least 2 characters long',
+      'maxlength': 'First name cannot be more than 25 characters long'
+    },
+    'telnum': {
+      'required': 'Tel. number is required.',
+      'pattern': 'Tel. number must contain only numbers.'
+    },
+    'email': {
+      'required': 'Email is required',
+      'email': 'Email not in valid format'
+    }
+  };
+
   // This enables us to get access to the template form ad the completely reset form
   @ViewChild('fform') feedbackFormDirective;
 
@@ -26,18 +56,60 @@ export class ContactComponent implements OnInit {
   ngOnInit() {
   }
 
+  // Create form 
   createForm() {
 
     // Create form with specified structure
     this.feedbackForm = this.fb.group({
-      firstname: ['', Validators.required],
-      lastname: ['', Validators.required],
-      telnum: [0, Validators.required],
-      email: ['', Validators.required],
+      firstname: ['', [Validators.required, Validators.minLength(2), Validators.maxLength(25)]],
+      lastname: ['', [Validators.required, Validators.minLength(2), Validators.maxLength(25)]],
+      telnum: [0, [Validators.required, Validators.pattern]],
+      email: ['', [Validators.required, Validators.email]],
       agree: [false, Validators.required],
       contacttype: ['None', Validators.required],
       message: ['', Validators.required]
     });
+
+    this.feedbackForm.valueChanges
+      .subscribe(data => this.onValueChanged(data));
+
+    // Reset validation messages
+    this.onValueChanged();
+
+  }
+
+  // ? in parameter: '?' means parameter is optional
+  onValueChanged(data?: any) {
+
+    if (!this.feedbackForm) {
+      return;
+    }
+
+    const form = this.feedbackForm;
+
+    for (const field in this.formErrors) {
+
+      // If formErrors object has values..
+      if (this.formErrors.hasOwnProperty(field)) {
+        // clear previous error
+        this.formErrors[field] = '';
+
+        const control = form.get(field);
+
+        // If form fields are invalid and dirty and is not null,
+        // extract error values and add into this.formError[field] key-value pair
+        if (control && control.dirty && !control.valid) {
+          const messages = this.validationMessages[field];
+          console.log(control.errors);
+          for (const key in control.errors) {
+            if (control.errors.hasOwnProperty(key)) {
+              this.formErrors[field] += messages[key] + ' ';
+            }
+          }
+        }
+      }
+
+    }
 
   }
 
